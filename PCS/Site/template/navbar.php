cc5757d5acf42df23dfdac561532a6ce8ee77768
Lucas Verrecchia <lverrecchia@myges.fr>
2024-06-29 18:33:14 +0200

    petite correction du chemin

--- PCS/Site/template/navbar.php
+++ PCS/Site/template/navbar.php
@@ -7,7 +7,7 @@ include (dirname(__DIR__) . "../../API/entities/isAuthenticated.php");
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #003366;">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="/../img/LOGO.png" alt="LOGO" width="70px"></a>
+        <a class="navbar-brand" href="#"><img src="../img/LOGO.png" alt="LOGO" width="70px"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
