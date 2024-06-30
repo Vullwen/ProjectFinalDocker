7a168400802ba1e39a036e6b91dde60cd6d817e6
Lucas Verrecchia <lverrecchia@myges.fr>
2024-06-30 15:14:36 +0200

    Changement confi.inc.php et header.php

--- PCS/API/database/conf.inc.php
+++ PCS/API/database/conf.inc.php
@@ -2,7 +2,7 @@
 
 
 define("DB_HOST", "localhost");
-define("DB_NAME", "bddpcs");
+define("DB_NAME", "BDDPCS");
 define("DB_PORT", "3306");
 define("DB_USER", "root");
 define("DB_PWD", "root");
--- PCS/Site/template/header.php
+++ PCS/Site/template/header.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paris Caretaker Services</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="http://localhost/2A-ProjetAnnuel/PCS/Site/style.css" rel="stylesheet">
+    <link href="/2A-ProjetAnnuel/PCS/Site/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://www.google.com/recaptcha/api.js" async defer>
     </script>
-    <script src="http://localhost/2A-ProjetAnnuel/PCS/Site/javascript/captcha.js"></script>
+    <script src="/2A-ProjetAnnuel/PCS/Site/javascript/captcha.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpEbABdaOHX1v3pteAwexNXWogevMN1xA"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
