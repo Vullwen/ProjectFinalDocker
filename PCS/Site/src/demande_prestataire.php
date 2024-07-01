54018753a2792521f2e6b10b22c4a4d9a6e8de48
Lucas Verrecchia <lverrecchia@myges.fr>
2024-07-01 13:30:28 +0200

    ajout mineur css et html

--- PCS/Site/src/demande_prestataire.php
+++ PCS/Site/src/demande_prestataire.php
@@ -3,21 +3,23 @@ include_once '../template/header.php';
 ?>
 
 <h1>Demande d'inscription comme Prestataire</h1>
-<form id="prestataireForm">
-    <label for="nom">Nom:</label><br>
-    <input type="text" id="nom" name="nom" required><br>
-    <label for="prenom">Prénom:</label><br>
-    <input type="text" id="prenom" name="prenom" required><br>
-    <label for="siret">Numéro SIRET:</label><br>
-    <input type="text" id="siret" name="siret" required><br>
-    <label for="adresse">Adresse:</label><br>
-    <input type="text" id="adresse" name="adresse" required><br>
-    <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email" required><br>
-    <label for="telephone">Téléphone:</label><br>
-    <input type="text" id="telephone" name="telephone" required><br>
-    <label for="domaine">Domaine:</label><br>
-    <input type="text" id="domaine" name="domaine" required><br>
-    <input type="submit" class="btn-primary" value="Envoyer">
-</form>
+<div class="container mt-5">
+    <form id="prestataireForm">
+        <label for="nom">Nom:</label><br>
+        <input type="text" id="nom" name="nom" required><br>
+        <label for="prenom">Prénom:</label><br>
+        <input type="text" id="prenom" name="prenom" required><br>
+        <label for="siret">Numéro SIRET:</label><br>
+        <input type="text" id="siret" name="siret" required><br>
+        <label for="adresse">Adresse:</label><br>
+        <input type="text" id="adresse" name="adresse" required><br>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br>
+        <label for="telephone">Téléphone:</label><br>
+        <input type="text" id="telephone" name="telephone" required><br>
+        <label for="domaine">Domaine:</label><br>
+        <input type="text" id="domaine" name="domaine" required><br>
+        <input type="submit" class="btn-primary" value="Envoyer">
+    </form>
+</div>
 <script src="../javascript/demande_prestataires.js"></script>
