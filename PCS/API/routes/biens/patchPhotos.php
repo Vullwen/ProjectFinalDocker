1f643a230b385f43f2878b301fb3235a786d23c5
Lucas Verrecchia <lverrecchia@myges.fr>
2024-07-03 21:48:41 +0200

    debug

--- PCS/API/routes/biens/patchPhotos.php
+++ PCS/API/routes/biens/patchPhotos.php
@@ -23,7 +23,7 @@ try {
 
     if ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
         $data = json_decode(file_get_contents('php://input'), true);
-        var_dump($_POST);
+        var_dump($_POST['photosToDelete']);
 
         if (!empty($data)) {
             $photosToDelete = $data['photosToDelete'];
