0e66d648d74cf9bf585b4354c7bba77ea69844a8
Lucas Verrecchia <lverrecchia@myges.fr>
2024-07-03 23:23:20 +0200

    debug ajout photos

--- PCS/Site/src/biens/modifierPhotos.php
+++ PCS/Site/src/biens/modifierPhotos.php
@@ -86,6 +86,7 @@ $photos = getPhotosBien($idBien);
         for (var i = 0; i < photos.length; i++) {
             formData.append('photos[]', photos[i]);
         }
+        console.loh(formData);
         fetch('http://51.75.69.184/2A-ProjetAnnuel/PCS/API/biens/photos?id=' + <?= json_encode($idBien) ?>, {
             method: 'PATCH',
             headers: {
