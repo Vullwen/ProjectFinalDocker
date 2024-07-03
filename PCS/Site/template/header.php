96d4c9c868493b2c33400266130d2d77ce3990d1
Lucas Verrecchia <lverrecchia@myges.fr>
2024-07-03 14:41:35 +0200

    Ajout script JS header pour caroussel

--- PCS/Site/template/header.php
+++ PCS/Site/template/header.php
@@ -16,6 +16,8 @@
     <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css' rel='stylesheet' />
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js'></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 
 
 
